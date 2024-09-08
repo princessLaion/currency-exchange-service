6778b3f6297897806e06797fec37b310233fbfc2
Lea Penarubia <princesslaion@gmail.com>
2024-09-08 18:32:26 +1000

    Rate Limiter and Bulkhead implementation

--- src/main/java/com/lrp/currency_exchange_service/controller/CircuitBreakerController.java
+++ src/main/java/com/lrp/currency_exchange_service/controller/CircuitBreakerController.java
@@ -1,13 +1,13 @@
 package com.lrp.currency_exchange_service.controller;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping(name = "/v1/circuit-breaker")
@@ -15,12 +15,17 @@ public class CircuitBreakerController {
 
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api-retry", fallbackMethod = "hardCodeResponse")
+    //@Re@try(name = "sample-api-retry", fallbackMethod = "hardCodeResponse")
+    @CircuitBreaker(name = "sample-api-retry", fallbackMethod = "hardCodeResponse")
+    @RateLimiter(name = "default")
+    @Bulkhead(name = "default") //how many concurrent calls
     public String sampleAPI() {
         logger.info("Sample API Received . . .");
-        ResponseEntity<String> forEntity = new RestTemplate()
-                .getForEntity("http://localhost:8080/nonExisting.html", String.class);
-        return forEntity.getBody();
+        //Comment for rate limiting and bulk head sample
+//        ResponseEntity<String> forEntity = new RestTemplate()
+//                .getForEntity("http://localhost:8080/nonExisting.html", String.class);
+//        return forEntity.getBody();
+        return "Sample API";
     }
 
     public String hardCodeResponse(Exception ex){
